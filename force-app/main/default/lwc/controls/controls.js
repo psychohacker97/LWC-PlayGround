76db955b6aa3ff0693a2e9e40e9aea6fb03cb49d
Sunandh Samala <sunandhsamala@gmail.com>
2026-05-16 21:25:31 +0530

    reverted line delete

--- force-app/main/default/lwc/controls/controls.js
+++ force-app/main/default/lwc/controls/controls.js
@@ -12,7 +12,7 @@ export default class Controls extends LightningElement {
     factors = [0,2,3,4,5,6];
     divisionFactors = [2,3,4,5,6];
     handleAdd(){
-        
+        this.dispatchEvent(new CustomEvent('add'));
     }
     handleSubtract(){
         this.dispatchEvent(new CustomEvent('subtract'));
